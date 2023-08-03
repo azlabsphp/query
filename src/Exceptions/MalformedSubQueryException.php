9c042fa33369c206ee18806974208edc65bb9785
azandrew-sidoine <azandrewdevelopper@gmail.com>
2023-08-03 12:32:24 +0000

    Moved Contracts files into a dedicated library

--- src/Exceptions/MalformedSubQueryException.php
+++ src/Exceptions/MalformedSubQueryException.php
@@ -2,19 +2,16 @@
 
 namespace Drewlabs\Query\Exceptions;
 
-use Exception;
-
-class MalformedSubQueryException extends Exception
+class MalformedSubQueryException extends QueryException
 {
     /**
      * Creates exception instance
      * 
      * @param string $query 
-     * @return void 
      */
     public function __construct(string $query)
     {
         $message = 'Expect subquery syntax to be method(p1, p2, p3, ...), but ' . ($query) . ' was passed instead';
-        parent::__construct($query);
+        parent::__construct($message);
     }
 }
